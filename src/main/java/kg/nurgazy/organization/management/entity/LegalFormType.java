a6cabf05ca210c5e56d7dedc51f3ff8b8c6d1ead
Nurgazy30 <nurgazy3012@gmail.com>
2022-08-18 02:26:08 +0600

    adding enums

---
+++ src/main/java/kg/nurgazy/organization/management/entity/LegalFormType.java
@@ -0,0 +1,7 @@
+package kg.nurgazy.organization.management.entity;
+
+public enum LegalFormType {
+    IP,
+    CHP,
+    OSOO
+}
---
+++ src/main/java/kg/nurgazy/organization/management/entity/PropertyFormType.java
@@ -0,0 +1,7 @@
+package kg.nurgazy.organization.management.entity;
+
+public enum PropertyFormType {
+    PRIVATE,
+    MUNICIPAL,
+    GOV
+}
